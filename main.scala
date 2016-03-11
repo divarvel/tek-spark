029c3a800adcd2c89be0ea026e1f81dc782f2baa
Clement Delafargue <clement@delafargue.name>
2016-03-11 15:43:23 +0100

    Display dummy data

--- main.scala
+++ main.scala
@@ -6,12 +6,16 @@ object Main {
   def main(args: Array[String]) = {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
+    sc.setLogLevel("WARN")
     val tontonsFile = "./data/tontons.txt"
     val classeFile = "./data/classe-americaine.txt"
 
     val tontonsData = sc.textFile(tontonsFile, 2).cache()
     val classeData = sc.textFile(classeFile, 2).cache()
 
-    println("test")
+    println("============================================================================")
+    println(classeData.first())
+    println("============================================================================")
+
   }
 }

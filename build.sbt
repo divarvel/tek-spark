739324d60833165c832c507308ea5f98e8f222c5
Clement Delafargue <clement@delafargue.name>
2016-03-11 15:28:45 +0100

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := """spark-test"""
+
+version := "1.0-SNAPSHOT"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "1.6.1"
---
+++ main.scala
@@ -0,0 +1,17 @@
+package main
+
+import org.apache.spark._
+
+object Main {
+  def main(args: Array[String]) = {
+    val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
+    val sc = new SparkContext(conf)
+    val tontonsFile = "./data/tontons.txt"
+    val classeFile = "./data/classe-americaine.txt"
+
+    val tontonsData = sc.textFile(tontonsFile, 2).cache()
+    val classeData = sc.textFile(classeFile, 2).cache()
+
+    println("test")
+  }
+}

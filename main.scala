71df521602de82de7b81d6e99bb1d39a86b5a48d
Clement Delafargue <clement@delafargue.name>
2016-11-04 15:50:14 +0100

    Spark examples

--- main.scala
+++ main.scala
@@ -1,8 +1,33 @@
 package main
 
 import org.apache.spark._
+import org.apache.spark.rdd._
 
 object Main {
+
+  def cleanRDD(x: RDD[String]): RDD[String] = {
+    val splitWords = (line: String) => {
+      line.split(' ').toList
+    }
+    val removePunctuationMarks = (word: String) => {
+      word
+        .replace(".", "")
+        .replace(",", "")
+        .replace("(", "")
+        .replace(")", "")
+    }
+
+    val isLongWord = (word: String) => word.length >= 6
+    val isName = (word: String) => word.endsWith(":")
+
+    x
+      .flatMap(splitWords)
+      .map(removePunctuationMarks)
+      .filter(isLongWord)
+      .filter(!isName(_))
+  }
+
+
   def main(args: Array[String]) = {
     val conf = new SparkConf().setAppName("Simple Application").setMaster("local")
     val sc = new SparkContext(conf)
@@ -13,9 +38,22 @@ object Main {
     val tontonsData = sc.textFile(tontonsFile, 2).cache()
     val classeData = sc.textFile(classeFile, 2).cache()
 
-    println("============================================================================")
-    println(classeData.first())
-    println("============================================================================")
+    val tontonsWords = cleanRDD(tontonsData)
+    val classeWords = cleanRDD(classeData)
+
+    val tontonsScore = tontonsWords.countByValue.toList
+    val highest = tontonsScore.sortBy(_._2).takeRight(10)
+
+    val classeScore = classeWords.countByValue.toList
+    val classeHighest = classeScore.sortBy(_._2).takeRight(20)
+
+    val common = tontonsWords.intersection(classeWords)
+    val commonScore = common.countByValue.toList
+    val commonHighest = commonScore.sortBy(_._2).takeRight(20)
+
+    println("=========================================================")
+    println(commonHighest)
+    println("=========================================================")
 
   }
 }
